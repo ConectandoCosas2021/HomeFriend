7283443e33a668eacddb8158989b671734435ea4
Ignacio Lanzani <72622016+TheUruguayo@users.noreply.github.com>
2021-10-23 21:33:26 -0300

    Video streaming

--- ejemplo_mqtt_esp8266-main/Principal/Principal.ino
+++ ejemplo_mqtt_esp8266-main/Principal/Principal.ino
@@ -1,12 +1,22 @@
 #include "thing_libreria.h"
 #include <ArduinoJson.h>
 #include "Esp32MQTTClient.h"
-
+#include <FS.h>
+#include <SPIFFS.h>
+#include "stream_library.h"
+#include "my_library.h"
 DynamicJsonDocument incoming_message(1024);
 WiFiClient espClient;
 PubSubClient client(espClient);
+WebServer server(80);
+OV2640 cam;
 
 #define MSG_BUFFER_SIZE  (50)
+#define CAMERA_MODEL_AI_THINKER
+#define timeit(label, code) code;
+void bring_resources();
+
+
 
 unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
@@ -42,16 +52,107 @@ void callback(char* topic, byte* payload, unsigned int length) {
      }
      }//end callback
 
-/* The SMTP Session object used for Email sending */
-//SMTPSession smtp;
+const char HEADER[] = "HTTP/1.1 200 OK\r\n" \
+                      "Access-Control-Allow-Origin: *\r\n" \
+                      "Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
+const char BOUNDARY[] = "\r\n--123456789000000000000987654321\r\n";
+const char CTNTTYPE[] = "Content-Type: image/jpeg\r\nContent-Length: ";
+const int hdrLen = strlen(HEADER);
+const int bdrLen = strlen(BOUNDARY);
+const int cntLen = strlen(CTNTTYPE);
 
-/* Callback function to get the Email sending status */
-//void smtpCallback(SMTP_Status status);
+ 
+void handle_jpg_stream(void) //loop infinito esto va a ir a un core
+{
+  char buf[32]; //creo que aca se guarda la imagen
+  int s;        //tamano de la imagen
+
+  WiFiClient client = server.client();
+
+  client.write(HEADER, hdrLen);
+  client.write(BOUNDARY, bdrLen);
+  
+  while (true)
+  {
+    if (!client.connected()) break;
+
+    cam.run(); //saca una nueva foto, el fb queda guardado en la variable de clase
+    s = cam.getSize();
+    client.write(CTNTTYPE, cntLen);
+    sprintf( buf, "%d\r\n\r\n", s );
+    client.write(buf, strlen(buf));
+    client.write((char *)cam.getfb(), s);
+    client.write(BOUNDARY, bdrLen);
+    Serial.print("stream en core :");
+    Serial.println(xPortGetCoreID());//core en el que se ejecuta
+  }
+}
+
+const char JHEADER[] = "HTTP/1.1 200 OK\r\n" \
+                       "Content-disposition: inline; filename=capture.jpg\r\n" \
+                       "Content-type: image/jpeg\r\n\r\n";
+const int jhdLen = strlen(JHEADER);
+//////////////////////////////////
+void handle_jpg(void)
+{
+  WiFiClient client = server.client();
+
+  cam.run();
+  if (!client.connected()) return;
+
+  client.write(JHEADER, jhdLen);
+  client.write((char *)cam.getfb(), cam.getSize());
+}
+/////////////////////////////////
+void handleNotFound()
+{
+  String message = "Server is running!\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  server.send(200, "text / plain", message);
+}
+//////////////////////SETUP////////////////////////////////////////////////
 const char* mqtt_server = "demo.thingsboard.io";
 void setup() {
   pinMode(2, OUTPUT);     //(LED AZUL)
   Serial.begin(115200);
-  setup_wifi();
+
+  /////
+  SPIFFS.begin(true);
+  delay(1000);
+  camera_config_t config;
+  set_camara_config(config);
+  cam.init(config);
+  set_motion_config();
+  //////wifi 2////
+  IPAddress ip;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID1, PWD1);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(F("."));
+  }
+  ip = WiFi.localIP();//local IP del server
+  Serial.println(F("WiFi connected"));
+  Serial.println("");
+  Serial.println(ip);
+  Serial.print("Stream Link: http://");
+  Serial.print(ip);
+  Serial.println("/mjpeg/1");
+
+  server.on("/mjpeg/1", HTTP_GET, handle_jpg_stream);
+  server.on("/jpg", HTTP_GET, handle_jpg);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  ///////////////////////////
+  //setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   iniciar_stmp();    
@@ -63,6 +164,8 @@ void loop(){
     reconnect(client);
   }
   client.loop();
+  server.handleClient();
+  
   cm = readUltrasonicDistance();
   unsigned long now = millis();
   if (now - lastMsg > 200) {
