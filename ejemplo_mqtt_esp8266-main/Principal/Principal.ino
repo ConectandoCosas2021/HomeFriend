0c21bfbfb53c1ff7e97aa69858c208181b17ac16
LuisPorras1997 <81394276+LuisPorras1997@users.noreply.github.com>
2021-11-13 15:28:52 -0300

    solo sensores

--- ejemplo_mqtt_esp8266-main/Principal/Principal.ino
+++ ejemplo_mqtt_esp8266-main/Principal/Principal.ino
@@ -1,24 +1,19 @@
 #include "thing_libreria.h"
 #include <ArduinoJson.h>
 #include "Esp32MQTTClient.h"
-#include <FS.h>
-#include <SPIFFS.h>
-#include "stream_library.h"
-#include "my_library.h"
 
-TaskHandle_t Task2;//LOOP INFINITO EN CORE 0
+
+
 DynamicJsonDocument incoming_message(1024);
+
 WiFiClient espClient;
 PubSubClient client(espClient);
-WebServer server(80);
-OV2640 cam;
 DHTesp dht;
 #define MSG_BUFFER_SIZE  (50)
-#define CAMERA_MODEL_AI_THINKER
-#define timeit(label, code) code;
+
 #define DHT_PIN 2
-#define pdTICKS_TO_MS( xTicks )   ( ( uint32_t ) ( xTicks ) * 1000 / configTICK_RATE_HZ )
-void bring_resources();
+/*#define pdTICKS_TO_MS( xTicks )   ( ( uint32_t ) ( xTicks ) * 1000 / configTICK_RATE_HZ )
+void bring_resources();*/
 
 
 
@@ -29,9 +24,6 @@ char msg2[MSG_BUFFER_SIZE];
 int value = 0;
 boolean estado = false;
 int cm = 0; //cm del sensor de ultra sonido
-bool mov_detected = false;
-TickType_t empezo=xTaskGetTickCount(); 
-
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -59,81 +51,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
      }
      }//end callback
 
-const char HEADER[] = "HTTP/1.1 200 OK\r\n" \
-                      "Access-Control-Allow-Origin: *\r\n" \
-                      "Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
-const char BOUNDARY[] = "\r\n--123456789000000000000987654321\r\n";
-const char CTNTTYPE[] = "Content-Type: image/jpeg\r\nContent-Length: ";
-const int hdrLen = strlen(HEADER);
-const int bdrLen = strlen(BOUNDARY);
-const int cntLen = strlen(CTNTTYPE);
 
- 
-void handle_jpg_stream(void) //loop infinito esto va a ir a un core
-{
-  char buf[32]; //creo que aca se guarda la imagen
-  int s;        //tamano de la imagen
-
-  WiFiClient client = server.client();
-
-  client.write(HEADER, hdrLen);
-  client.write(BOUNDARY, bdrLen);
-  bool entro=false;
-  bool primera=true;
-  while (client.connected())
-  { entro=true;
-    if (primera){
-      esp_camera_deinit();//des inicializa la camara
-      camera_config_t config;
-      set_camara_config(config);
-      cam.init(config);
-      primera=false;
-      }
-    cam.run(); //saca una nueva foto, el fb queda guardado en la variable de clase
-    s = cam.getSize();
-    client.write(CTNTTYPE, cntLen);
-    sprintf( buf, "%d\r\n\r\n", s );
-    client.write(buf, strlen(buf));
-    client.write((char *)cam.getfb(), s);
-    client.write(BOUNDARY, bdrLen);
-    Serial.print("stream en core :");
-    Serial.println(xPortGetCoreID());//core en el que se ejecuta
-  }
-  if (entro){
-    esp_camera_deinit();//des inicializa la camara
-    camera_config_t config; //crea una config nueva
-    set_camara_config2(config); //configura para el motion sensor
-    Serial.println("incia la camara");
-    cam.init(config);//inica la camara
-    }
-}
-
-const char JHEADER[] = "HTTP/1.1 200 OK\r\n" \
-                       "Content-disposition: inline; filename=capture.jpg\r\n" \
-                       "Content-type: image/jpeg\r\n\r\n";
-const int jhdLen = strlen(JHEADER);
-//////////////////////////////////
-void handle_jpg(void)
-{
-  WiFiClient client = server.client();
-  cam.run();
-  if (!client.connected()) return;
-  client.write(JHEADER, jhdLen);
-  client.write((char *)cam.getfb(), cam.getSize());
-}
-/////////////////////////////////
-void handleNotFound()
-{
-  String message = "Server is running!\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  server.send(200, "text / plain", message);
-}
 //////////////////////SETUP////////////////////////////////////////////////
 const char* mqtt_server = "demo.thingsboard.io";
 void setup() {
@@ -143,71 +61,9 @@ void setup() {
   /////
   dht.setup(DHT_PIN, DHTesp::DHT11);
   /////
-  SPIFFS.begin(true);
-  delay(1000);
-  set_motion_config();
+  setup_wifi();
   
-  //////wifi 2////
-  IPAddress ip;
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID1, PWD1);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(F("."));
-  }
-  ip = WiFi.localIP();//local IP del server
-  Serial.println(F("WiFi connected"));
-  Serial.println("");
-  Serial.println(ip);
-  Serial.print("Stream Link: http://");
-  Serial.print(ip);
-  Serial.println("/mjpeg/1");
-
-  server.on("/mjpeg/1", HTTP_GET, handle_jpg_stream);
-  server.on("/jpg", HTTP_GET, handle_jpg);
-  server.onNotFound(handleNotFound);
-  server.begin();
-  ///////////////////////////
-  //setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  iniciar_stmp();    
- /////////////////////////////////////////////////////////////////   
-
-  camera_config_t config;
-  set_camara_config2(config);
-  Serial.println("incia la camara");
-  cam.init(config);
-
-///
-    xTaskCreatePinnedToCore(
-    Task2code,   /* Task function. */
-    "Task2",     /* name of task. */
-    10000,       /* Stack size of task */
-    NULL,        /* parameter of the task */
-    1,           /* priority of the task */
-    &Task2,      /* Task handle to keep track of created task */
-    0);          /* pin task to core 1 */
-  delay(500);
-}
-
-void Task2code( void * pvParameters ) {// core 0
-  
-  for (;;) { // infinite loop
-//  Serial.print("task2 :");
-//  Serial.println(xPortGetCoreID());//core en el que se ejecuta
-  server.handleClient();
-  cam.run();//guarda la foto en el buffer     
-  procces_buffer(cam.getfb()); //camputra frames
-  hay_movimiento(mov_detected); //detecta si hay o no movimiento  
-  if (mov_detected){
-  Serial.print("valor de bandera fuera del loop" );Serial.println(mov_detected);  
-  }
-  }
-  }
-
+} 
 void loop(){
   
   if (!client.connected()) {//conexion a thingsboard
@@ -217,12 +73,10 @@ void loop(){
   
   //cm = readUltrasonicDistance();
   cm = 22; //Esto hay que sacarlo cuando metamos el sensor de distancia  
-  
-  
 
   
-  if (pdTICKS_TO_MS(xTaskGetTickCount() - empezo)> 20000) {
-    empezo = xTaskGetTickCount();
+  if ( millis() - lastMsg > 20000) { //TODO Poner millis
+    lastMsg = millis();
     TempAndHumidity lastValues = dht.getTempAndHumidity();//Tomo la temperatura   
     int temp = (int)lastValues.temperature;
     int hum = (int)lastValues.humidity;
@@ -239,11 +93,5 @@ void loop(){
         snprintf (msg, MSG_BUFFER_SIZE, "{'humedad': %ld}", hum);
         client.publish("v1/devices/me/telemetry", msg);
       }
-      
-    snprintf (msg, MSG_BUFFER_SIZE, "{'movimiento': %ld}", mov_detected);
-    Serial.print("valor de bandera fuera antes de enviar" );Serial.println(mov_detected);  
-    mov_detected=false;
-    client.publish("v1/devices/me/telemetry", msg);
-  
     }
 }//end loop
--- ejemplo_mqtt_esp8266-main/Principal/thing_libreria.cpp
+++ ejemplo_mqtt_esp8266-main/Principal/thing_libreria.cpp
@@ -1,12 +1,11 @@
 #include "thing_libreria.h"
 
 const int laserPin = 12;
-const int triggerPin = 15;
-const int echoPin = 14;
-SMTPSession smtp;
+//const int triggerPin = 15;
+//const int echoPin = 14;
 const char* ssid = "perdedores";
 const char* password = "somos perdedores 1375";
-const char* token = "WCw44NcuFq0tT0eVRQ5l";
+const char* token = "WCw44NcuFq0tT0eVRQ5l"; //Fijarse nuevo token
 ////////////////////// setup wifi///////////////////////////////////////
 void setup_wifi() {
 
@@ -31,78 +30,8 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
-////////////////////MAIL/////////////////////////////////////////
-void setup_session(ESP_Mail_Session &session){
-  session.server.host_name = SMTP_HOST;
-  session.server.port = SMTP_PORT;
-  session.login.email = AUTHOR_EMAIL;
-  session.login.password = AUTHOR_PASSWORD;
-  session.login.user_domain = "";
-}
 
-void setup_message(SMTP_Message &message){
-  /* Set the message headers */
-  message.sender.name = "HOMEFRIEND";
-  message.sender.email = AUTHOR_EMAIL;
-  message.subject = "HomeFriend sistema de alertas";
-  message.addRecipient("Luis", RECIPIENT_EMAIL);
-  /*Send HTML message*/
-  String htmlMsg = "<div style=\"color:#2f4468;\"><h1>HomeFriend</h1><p>- Este es un mensaje enviado desde la ESP32 de prueba</p></div>";
-  message.html.content = htmlMsg.c_str();
-  message.html.content = htmlMsg.c_str();
-  message.text.charSet = "us-ascii";
-  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;  
-  /* Set the custom message header */
-  //message.addHeader("Message-ID: <abcde.fghij@gmail.com>");
-
-}
-
-
-void iniciar_stmp(){
-  
-  smtp.debug(1);  
-  smtp.callback(smtpCallback);  
-  ESP_Mail_Session session;
-  setup_session(session); 
-  SMTP_Message message;
-  setup_message(message);  
-  /* Connect to server with the session config */
-  if (!smtp.connect(&session))
-    return;
-  /* Start sending Email and close the session */
-  if (!MailClient.sendMail(&smtp, &message))
-    Serial.println("Error sending Email, " + smtp.errorReason());
-    //Fin setup para Mail
-  }
-
-void smtpCallback(SMTP_Status status){
-  /* Print the current status */
-  Serial.println(status.info());
-
-  /* Print the sending result */
-  if (status.success()){
-    Serial.println("----------------");
-    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
-    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
-    Serial.println("----------------\n");
-    struct tm dt;
-
-    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
-      /* Get the result item */
-      SMTP_Result result = smtp.sendingResult.getItem(i);
-      time_t ts = (time_t)result.timestamp;
-      localtime_r(&ts, &dt);
-
-      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
-      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
-      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
-      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients);
-      ESP_MAIL_PRINTF("Subject: %s\n", result.subject);
-    }
-    Serial.println("----------------\n");
-  }
-}
-/////////////////////////////////////////////////////////////
+/*
 long readUltrasonicDistance(){
   pinMode(triggerPin, OUTPUT);  // Clear the trigger
   digitalWrite(triggerPin, LOW);
@@ -115,6 +44,7 @@ long readUltrasonicDistance(){
   // Reads the echo pin, and returns the sound wave travel time in microseconds
   return 0.01723*pulseIn(echoPin, HIGH);
 }
+*/
 ////////////prende apaga led/////////////////////////////////////
 void setBlueLed(bool value){
   pinMode(laserPin, OUTPUT);
@@ -128,6 +58,7 @@ void setBlueLed(bool value){
 }
 }
 ////////////END prende apaga led/////////////////////////////////////
+
 ////////////Reconectarse a thingsboard/////////////////////////////////////
 void reconnect(PubSubClient &client) {//reconectarse a thingsboard
   // Loop until we're reconnected
@@ -141,7 +72,7 @@ void reconnect(PubSubClient &client) {//reconectarse a thingsboard
       // ... and resubscribe to rpc topic
       client.subscribe("v1/devices/me/rpc/request/+");
     } else {
-      Serial.print("failed, rc=");
+      Serial.println("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
